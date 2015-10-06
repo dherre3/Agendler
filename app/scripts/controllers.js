652c0601c63e6824bfcdf5ba29d36fd993e4966e
David Herrera <davidfherrerar@gmail.com>
2015-10-05 23:30:51 -0400

    First commit

---
+++ app/scripts/controllers.js
@@ -0,0 +1,95 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+
+
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+
+  };
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+.controller('LoginCtrl', function($scope, $stateParams,$state) {
+  $scope.signin=function(){
+    console.log('boom');
+    var email=$scope.signin.email;
+    var password=$scope.signin.password;
+    var ref=new Firebase('https://blazing-inferno-1723.firebaseio.com');
+    ref.authWithPassword({
+      email:email,
+      password:password
+    },function(error, authData){
+      if(error){
+        throw error;
+      }else{
+        console.log(authData);
+        $state.go('app.playlists');
+      }
+    });
+  };
+})
+.controller('SignUpCtrl', function($scope, $stateParams,$state) {
+  
+})
+.controller('AppFormsCtrl', function($scope, $stateParams,$state) {
+
+  
+});
